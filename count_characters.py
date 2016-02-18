2c44deec9ae3978a6c2e2f925861b0086f35049e
singhpawan <pawandeep.me@gmail.com>
2016-02-18 16:10:22 -0500

    Pushing the count_character which outputs the character frequency of any file

---
+++ count_characters.py
@@ -0,0 +1,43 @@
+# Count the character frequency of the letters in Resume
+
+import pandas as pd
+from ggplot import *
+from collections import Counter
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import os
+
+# Plot the histogram and the character count for the resume
+def get_character_frequency(file):
+    text = open(file).read().lower()
+    count = Counter()
+    for i in range(len(text)):
+        c = text[i]
+        if c in "abcdefghijklmnopqrstuvwxyz":
+            count[c] += 1
+    count = dict(count)
+    labels = []
+    values = []
+    for k,v in count.items():
+        labels.append(k)
+        values.append(v)
+    pos = np.arange(len(labels))
+    width = 1.0
+    ax = plt.axes()
+    ax.set_xticks(pos + (width / 2))
+    ax.set_xticklabels(labels)
+    plt.bar(pos, values, width, color='r')
+    plt.show()
+    print count
+
+    return count
+
+# Take the input file and call the get_character_frequency function 
+def main():
+    file = sys.argv[1]
+    get_character_frequency(file)
+
+# Boiler Plate code to call the main function
+if __name__ == '__main__':
+    main()
